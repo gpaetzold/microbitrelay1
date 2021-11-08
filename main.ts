6039acdcec142309001fdf7d9bcdf7cc07d3fca4
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-11-08 11:55:45 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(2000)
 })
